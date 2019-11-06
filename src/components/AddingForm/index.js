cb9ee58b5a14da5dd88c84141496230b0fb45f45
Валерий Кулаков <kulakov.resurs@tut.by>
2019-11-06 14:55:34 +0300

    form validate synch
    Form validate synch added

--- src/components/AddingForm/index.js
+++ src/components/AddingForm/index.js
@@ -5,67 +5,82 @@ import './style.css';
 
 const component = 'adding-form';
 
+
+const validate = values => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'Name is required!';
+    }
+    if (!values.surname) {
+        errors.surname = 'Surname is required!';
+    }
+    if (!values.age) {
+        errors.age = 'Age is required!';
+    } else if (values.age < 0 || values.age > 100) {
+        errors.age = 'Incorrect age (must be between 0 and 100)!';
+    }
+    return errors;
+}
+
+
+const renderField = (props) => {
+    const { input, type, placeholder, meta: { touched, error } } = props;
+    return (
+        <>
+            <input {...input} placeholder={placeholder} type={type}/>
+            {touched && ((error && <span className={`${component}__message`}>{error}</span>))}
+        </>
+    )
+}
+
+
 class AddingForm extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {};
         this.onSubmit = this.onSubmit.bind(this);
     }
     
     onSubmit(values) {
         const { reset } = this.props;
-        
-        if (values.name &&
-            values.surname &&
-            values.age &&
-            !isNaN(values.age)) {
-            if (values.age > 0 && values.age <100) {
-                this.setState({valid: true, message: 'Data is being sent...'});
-                this.props.dispatch({
-                    type: 'POST_PERSON',
-                    payload: values
-                });
-                reset();
-            } else {
-                this.setState({valid: false, message: 'Incorrect age!'});
-            }
-        } else {
-            this.setState({valid: false, message: 'All fields required!'});
-        }
+        this.props.dispatch({
+            type: 'POST_PERSON',
+            payload: values
+        });
+        reset();
     }
     
     render() {
         const { handleSubmit, pristine, reset, submitting } = this.props;
-        const { valid, message } = this.state;
         return (
             <form onSubmit={handleSubmit(this.onSubmit)} className={component}>
                 <Field
-                    name='name'
-                    component='input'
-                    type='text'
+                    name="name"
+                    type="text"
                     placeholder='Name*'
+                    component={renderField}
                 />
                 <Field
-                    name='surname'
-                    component='input'
-                    type='text'
+                    name="surname"
+                    type="text"
                     placeholder='Surname*'
+                    component={renderField}
                 />
                 <Field
-                    name='age'
-                    component='input'
-                    type='number'
+                    name="age"
+                    type="number"
                     placeholder='Age*'
+                    component={renderField}
                 />
                 <button type='submit' disabled={pristine || submitting}>Add</button>
                 <button type='button' disabled={pristine || submitting} onClick={reset}>Clear form</button>
-                {!valid && <div className={`${component}__message`}>{message}</div>}
             </form>
         );
     }
 }
 
+
 export default reduxForm ({
-    form: 'adding_person'
+    form: 'adding_person',
+    validate
 })(AddingForm);
